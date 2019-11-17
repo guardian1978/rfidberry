cbfc2d8f9b724b5da8786dce0048b094871aae32
Carsten <guardian2@gmx.de>
2019-11-17 14:33:06 +0100

    initial commit

---
+++ inventary.py
@@ -0,0 +1,9 @@
+#!/usr/bin(python
+# coding: utf8
+albums = {
+'7E00131FFF8D': 'die-kinder-aus-der-krachmacherstrasse.pls',
+'7E00134EBC9F': 'Eiskönigin',
+'7E001317473D': 'wir-kinder-aus-dem-moewenweg.pls',
+'010A3ACD3AC6': '30-beste-kindergartenlieder.pls'
+}
+test = 12
