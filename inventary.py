2e13a3c57bfbd53f74344f95ff7e5d8175c7a143
Claudia Wagner <claudiawagner@ClaudianersiMac.fritz.box>
2020-03-21 13:25:17 +0100

    update

--- inventary.py
+++ inventary.py
@@ -2,9 +2,10 @@
 # coding: utf8
 albums = {
 '7E00131FFF8D': 'die-kinder-aus-der-krachmacherstrasse.pls',
-'7E00134EBC9F': 'Eiskönigin',
+'7E00134EBC9F': 'tompalme.pls',
 '7E001317473D': 'wir-kinder-aus-dem-moewenweg.pls',
 '010A3ACD3AC6': '30-beste-kindergartenlieder.pls',
 '64009CAA82D0': 'karokarottepony.pls'
+'5F00BFD5E7D2': 'sommer-im-moewenweg.pls'
 }
 test = 12
